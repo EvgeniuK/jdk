38be4ecf7d0de025c25b737732f6bf37c84c7b70
EvgeniuK <93664681+EvgeniuK@users.noreply.github.com>
2021-11-07 19:25:37 +0300

    add main class1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+//      Задача 1
+
+        int[] Fib = new int[7];
+
+       Fib[0] = 0;
+       Fib[1] = 1;
+       for (int i = 2; i < Fib.length; ++i) {
+           Fib[i] = Fib[i - 1] + Fib[i - 2];
+           System.out.println(Fib[i]);
+
     }
+    }
+
 }
