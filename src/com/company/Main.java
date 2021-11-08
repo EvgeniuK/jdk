1e506b48b8b2830ba89309de7040d11a33ffc2b0
EvgeniuK <93664681+EvgeniuK@users.noreply.github.com>
2021-11-08 18:17:03 +0300

    add 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-//      Задача 1
-
-        int[] Fib = new int[7];
-
-       Fib[0] = 0;
-       Fib[1] = 1;
-       for (int i = 2; i < Fib.length; ++i) {
-           Fib[i] = Fib[i - 1] + Fib[i - 2];
-           System.out.println(Fib[i]);
+//     Задача 1
+//     Scanner Sw = new Scanner(System.in);
+//     int n = Sw.nextInt();
+//     int[] Fib = new int[n];
+//     Fib[0] = 0;
+//     Fib[1] = 1;
+//      for (int i = 2; i < Fib.length; ++i) {
+//        Fib[i] = Fib[i - 1] + Fib[i - 2];
+//        System.out.println(Fib[i]);
 
            //        Задача 2
 
@@ -23,41 +23,49 @@ public class Main {
 //        for(int i = 0; i < mas.length; i++) {
 //            if(mas[i] > max)
 //                max = mas[i];
-
 //            if(mas[i] < min)
 //                min = mas[i];
 //        }
 //        System.out.println("Максимальное число: " + max);
 //        System.out.println("Минимальное число: " + min);
+//           System.out.println("Сумма:" + max+ min);
 
 
 
            //        Задача 3.
 
-//        int[] mas2 = {1, 5, 3, 7, 2, 6, 2, 4, 7};
-//        int sum = 0;
-//        int sum1 = 0;
-//         sum = mas2[0]+mas2[2]+mas2[4]+mas2[6]+mas2[8];
-//        sum1 = mas2[1]+mas2[3]+mas2[5]+mas2[7];
-//        System.out.println(sum1-sum);
+//           int[] mas2 = {1, 5, 3, 7, 2, 6, 2, 4, 7};
+//       int sum = 0;
+//       int sum1 = 0;
+//       for (int i = 0; i < mas2.length; i++){
+//           if (i%2==0){
+//               sum  += mas2[i];
+//           }
+//           if (i%2==1){
+//               sum1  += mas2[i];
+//       }
+//       }
+//       System.out.println(sum1-sum);
 
 //        Задача 4
-
-           int[] mas3 = {1, 5, 3, 7, 3, 6, 3, 4, 7};
-           int a = 0;
-           int d = 0;
-           for(int i = 0; i < mas3.length; i++){
-               for(int j = 0; j < mas3.length-1; j++){
-                   if (mas3[i] == mas3[j]){
-                       a = mas3[i];
-                       d++;
-                   }
-               }
-               System.out.println("элемент "+a+" встречается "+d+" раз");
-               d = 0;
+//
+  //       int[] mas3 = {1, 5, 3, 7, 3, 6, 3, 4, 7};
+//
+  //       int a = 0;
+  //       int d = 0;
+  //       for(int i = 0; i < mas3.length; i++){
+  //           for(int j = 0; j < mas3.length-1; j++){
+  //               if (mas3[i] == mas3[j]) {
+  //                   a = mas3[i];
+  //                   d++;
+  //               }
+  //               }
+  //           }
+  //           System.out.println("элемент "+a+" встречается "+d+" раз");
+  //           d = 0;
 
 
            }
     }
 
-}
+
