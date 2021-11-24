fe7dbbf4c21f7f2aeb1994889b2ed9c1c61450a1
EvgeniuK <93664681+EvgeniuK@users.noreply.github.com>
2021-11-24 23:28:20 +0300

    les3

--- src/com/TMS/Less3/Less3.java
+++ src/com/TMS/Less3/Less3.java
@@ -7,65 +7,64 @@ public class Less3 {
     public static void main(String[] args) {
 
 //     Задача 1
-     Scanner Sw = new Scanner(System.in);
-     int n = Sw.nextInt();
-     int[] Fib = new int[n];
-     Fib[0] = 0;
-     Fib[1] = 1;
-      for (int i = 2; i < Fib.length; ++i) {
-        Fib[i] = Fib[i - 1] + Fib[i - 2];
-        System.out.println(Fib[i]);
+        Scanner Sw = new Scanner(System.in);
+        int n = Sw.nextInt();
+        int[] Fib = new int[n];
+        Fib[0] = 0;
+        Fib[1] = 1;
+        for (int i = 2; i < Fib.length; ++i) {
+            Fib[i] = Fib[i - 1] + Fib[i - 2];
+            System.out.println(Fib[i]);
 
-           //        Задача 2
+            //        Задача 2
 
-//        int[] mas = {1, 5, 3, 7, 2, 6, 2};
-//        int max = mas[0], min = mas[0];
-//        for(int i = 0; i < mas.length; i++) {
-//            if(mas[i] > max)
-//                max = mas[i];
-//            if(mas[i] < min)
-//                min = mas[i];
-//        }
-//        System.out.println("Максимальное число: " + max);
-//        System.out.println("Минимальное число: " + min);
-//           System.out.println("Сумма:" + max+ min);
-
-
-
-           //        Задача 3.
-
-//           int[] mas2 = {1, 5, 3, 7, 2, 6, 2, 4, 7};
-//       int sum = 0;
-//       int sum1 = 0;
-//       for (int i = 0; i < mas2.length; i++){
-//           if (i%2==0){
-//               sum  += mas2[i];
-//           }
-//           if (i%2==1){
-//               sum1  += mas2[i];
-//       }
-//       }
-//       System.out.println(sum1-sum);
-
-//        Задача 4
-//       int[] mas3 = {1, 5, 3, 7, 3, 6, 3, 4, 7};
-//
-  //       int a = 0;
-  //       int d = 0;
-  //       for(int i = 0; i < mas3.length; i++){
-  //           for(int j = 0; j < mas3.length-1; j++){
-  //               if (mas3[i] == mas3[j]) {
-  //                   a = mas3[i];
-  //                   d++;
-  //               }
-  //               }
-  //           }
-  //           System.out.println("элемент "+a+" встречается "+d+" раз");
-  //           d = 0;
+            int[] mas = {1, 5, 3, 7, 2, 6, 2};
+            int max = mas[0], min = mas[0];
+            for (int i = 0; i < mas.length; i++) {
+                if (mas[i] > max)
+                    max = mas[i];
+                if (mas[i] < min)
+                    min = mas[i];
+            }
+            System.out.println("Максимальное число: " + max);
+            System.out.println("Минимальное число: " + min);
+            System.out.println("Сумма:" + max + min);
 
 
-           }
+            //        Задача 3.
+
+            int[] mas2 = {1, 5, 3, 7, 2, 6, 2, 4, 7};
+            int sum = 0;
+            int sum1 = 0;
+            for (int i = 0; i < mas2.length; i++) {
+                if (i % 2 == 0) {
+                    sum += mas2[i];
+                }
+                if (i % 2 == 1) {
+                    sum1 += mas2[i];
+                }
+            }
+            System.out.println(sum1 - sum);
+
+//               Задача 4
+            int[] mas3 = {1, 5, 3, 7, 3, 6, 3, 4, 7};
+
+            int a = 0;
+            int d = 0;
+            for (int i = 0; i < mas3.length; i++) {
+                for (int j = 0; j < mas3.length - 1; j++) {
+                    if (mas3[i] == mas3[j]) {
+                        a = mas3[i];
+                        d++;
+                    }
+                }
+            }
+            System.out.println("элемент " + a + " встречается " + d + " раз");
+            d = 0;
+
+
+        }
     }
-    }
+}
 
 
