3db338982d5e17ef981770e608c5be0e258d9534
EvgeniuK <93664681+EvgeniuK@users.noreply.github.com>
2021-11-30 19:00:28 +0300

    Add Les Collection

--- src/com/TMS/Less4/Main.java
+++ src/com/TMS/Less4/Main.java
@@ -5,17 +5,17 @@ import java.time.Month;
 public class Main {
 
     public static void main(String[] args) {
-        //    Mants mants = Mants.APR;
-        //      switch (mants){
-        //          case APR:
-        //              System.out.println("SPRING");
-        //              break;
+            Mants mants = Mants.APR;
+              switch (mants){
+                  case APR:
+                      System.out.println("SPRING");
+                      break;
 
-        //          case DEC:
-        //              System.out.println("WINTER");
-        //              break;
+                  case DEC:
+                      System.out.println("WINTER");
+                      break;
 
-        //      }
+              }
 
         AR ar = new AR(new int[]{1, 4, 6, 8, 3, 0});
 //        System.out.println( ar.SearchMin());
